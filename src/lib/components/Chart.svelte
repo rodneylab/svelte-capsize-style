6d8931a3ee8f90c5ceddf0750efaf70a690b2cd3
Rodney Lab <codesign@rodneylab.com>
2022-09-27 08:16:15 +0100

    style: 🛁  switch to tab indentation

--- src/lib/components/Chart.svelte
+++ src/lib/components/Chart.svelte
@@ -1,27 +1,27 @@
 <script>
-  import Line from '$lib/components/Line.svelte';
+	import Line from '$lib/components/Line.svelte';
 
-  /** @type {boolean} */
-  export let withCapsize;
+	/** @type {boolean} */
+	export let withCapsize;
 
-  const capHeights = [16, 20, 25, 31, 39, 48, 61];
+	const capHeights = [16, 20, 25, 31, 39, 48, 61];
 </script>
 
 <div class="container">
-  <Line {withCapsize} capHeight={capHeights[6]}>E</Line>
-  <Line {withCapsize} capHeight={capHeights[5]}>H S T</Line>
-  <Line {withCapsize} capHeight={capHeights[4]}>K N C</Line>
-  <Line {withCapsize} capHeight={capHeights[3]}>P J B S A</Line>
-  <Line {withCapsize} capHeight={capHeights[2]}>T R S N O</Line>
-  <Line {withCapsize} capHeight={capHeights[1]}>A S O T N</Line>
-  <Line {withCapsize} capHeight={capHeights[0]}>Y F H S D</Line>
+	<Line {withCapsize} capHeight={capHeights[6]}>E</Line>
+	<Line {withCapsize} capHeight={capHeights[5]}>H S T</Line>
+	<Line {withCapsize} capHeight={capHeights[4]}>K N C</Line>
+	<Line {withCapsize} capHeight={capHeights[3]}>P J B S A</Line>
+	<Line {withCapsize} capHeight={capHeights[2]}>T R S N O</Line>
+	<Line {withCapsize} capHeight={capHeights[1]}>A S O T N</Line>
+	<Line {withCapsize} capHeight={capHeights[0]}>Y F H S D</Line>
 </div>
 
 <style>
-  .container {
-    padding: var(--spacing-12) var(--spacing-4);
-    background-color: var(--background-colour);
-    width: var(--max-width-full);
-    height: 100%;
-  }
+	.container {
+		padding: var(--spacing-12) var(--spacing-4);
+		background-color: var(--background-colour);
+		width: var(--max-width-full);
+		height: 100%;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,65 +1,65 @@
 <script>
-  import Chart from '$lib/components/Chart.svelte';
-  import '$lib/styles/styles.css';
-  import '@fontsource/abril-fatface';
-  import '@fontsource/courier-prime';
+	import Chart from '$lib/components/Chart.svelte';
+	import '$lib/styles/styles.css';
+	import '@fontsource/abril-fatface';
+	import '@fontsource/courier-prime';
 </script>
 
 <header><h1>Svelte Capsize Styling</h1></header>
 <main class="content">
-  <section class="with">
-    <h2 class="heading">With Capsize</h2>
-    <Chart --background-colour="hsl(336deg 85% 54%)" withCapsize={true} />
-  </section>
-  <section class="without">
-    <h2 class="heading">Without Capsize</h2>
-    <Chart --background-colour="hsl(340deg 70% 70%)" withCapsize={false} />
-  </section>
+	<section class="with">
+		<h2 class="heading">With Capsize</h2>
+		<Chart --background-colour="hsl(336deg 85% 54%)" withCapsize={true} />
+	</section>
+	<section class="without">
+		<h2 class="heading">Without Capsize</h2>
+		<Chart --background-colour="hsl(340deg 70% 70%)" withCapsize={false} />
+	</section>
 </main>
 
 <style>
-  header {
-    text-align: center;
-    margin: var(--spacing-12) var(--spacing-6) var(--spacing-0);
-  }
+	header {
+		text-align: center;
+		margin: var(--spacing-12) var(--spacing-6) var(--spacing-0);
+	}
 
-  .content {
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-areas:
-      'with'
-      'without';
-    width: min(var(--max-width-full) - var(--spacing-12), var(--max-width-wrapper));
-    overflow-x: hidden;
-    margin: var(--spacing-0) auto var(--spacing-24);
-    overflow-y: hidden;
-  }
+	.content {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-areas:
+			'with'
+			'without';
+		width: min(var(--max-width-full) - var(--spacing-12), var(--max-width-wrapper));
+		overflow-x: hidden;
+		margin: var(--spacing-0) auto var(--spacing-24);
+		overflow-y: hidden;
+	}
 
-  .heading {
-    width: var(--max-width-full);
-    text-align: center;
-  }
-  .with,
-  .without {
-    margin: var(--spacing-6) auto;
-    width: var(--max-width-full);
-  }
-  .with {
-    grid-area: with;
-  }
-  .without {
-    grid-area: without;
-  }
+	.heading {
+		width: var(--max-width-full);
+		text-align: center;
+	}
+	.with,
+	.without {
+		margin: var(--spacing-6) auto;
+		width: var(--max-width-full);
+	}
+	.with {
+		grid-area: with;
+	}
+	.without {
+		grid-area: without;
+	}
 
-  @media screen and (min-width: 64rem) {
-    .content {
-      grid-template-columns: 1fr 1fr;
-      grid-template-areas: 'with without';
-    }
+	@media screen and (min-width: 64rem) {
+		.content {
+			grid-template-columns: 1fr 1fr;
+			grid-template-areas: 'with without';
+		}
 
-    .with,
-    .without {
-      margin: var(--spacing-0) auto;
-    }
-  }
+		.with,
+		.without {
+			margin: var(--spacing-0) auto;
+		}
+	}
 </style>
